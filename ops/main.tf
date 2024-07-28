23aa4e0bb5f78b12113ed80e282b8dc1faeb0743
srinivas-banoth-dev <srinivas.banoth.dev@gmail.com>
2024-07-28 22:47:40 +0530

    REST API

--- ops/main.tf
+++ ops/main.tf
@@ -4,6 +4,7 @@ provider "aws" {
   access_key = "test"
   secret_key = "test"
   #   allowed_account_ids = ["000000000000"]
+  # allowed_account_ids = [000000000000]
   #   s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
@@ -18,148 +19,8 @@ provider "aws" {
     elbv2 = "http://localhost:4566"
     ec2   = "http://localhost:4566"
     eks   = "http://localhost:4566"
+    logs           = "http://localhost:4566"
   }
-}
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-}
-
-resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-}
-
-resource "aws_security_group" "alb_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_lb" "alb" {
-  name               = "my-alb"
-  internal           = false
-  load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
-  subnets            = [aws_subnet.subnet.id]
-}
-
-resource "aws_lb_target_group" "tg" {
-  name     = "my-targets"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
-}
-
-resource "aws_lb_listener" "listener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
-  }
-}
-
-resource "aws_instance" "myec2" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
-  key_name      = "my-key"
-
-  tags = {
-    Name = "MyEC2Instance"
-  }
-}
-# resource "aws_lb" "myalb" {
-#   name               = "my-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = ["sg-12345678"]
-#   subnets            = ["subnet-12345678", "subnet-87654321"]
-
-#   enable_deletion_protection = false
-# }
-
-resource "aws_lb_target_group" "mytargetgroup" {
-  name     = "my-targets"
-  port     = 80
-  protocol = "HTTP"
-  vpc_id   = "vpc-12345678"
-}
-
-resource "aws_lb_listener" "mylistener" {
-  load_balancer_arn = aws_lb.alb.arn
-  port              = 80
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.mytargetgroup.arn
-  }
-}
-
-
-# module "eks" {
-#    source  = "terraform-aws-modules/eks/aws"
-#   version = "20.15.0"
-#   cluster_name    = "my-cluster"
-#   cluster_version = "1.21"
-#   subnets         = ["subnet-12345678", "subnet-87654321"]
-#   vpc_id          = "vpc-12345678"
-
-#   node_groups = {
-#     eks_nodes = {
-#       desired_capacity = 2
-#       max_capacity     = 3
-#       min_capacity     = 1
-
-#       instance_type = "t3.medium"
-#     }
-#   }
-# }
-resource "aws_iam_role" "example" {
-  name = "example-role"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "eks.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
-}
-resource "aws_eks_cluster" "example" {
-  name     = "example-cluster"
-  role_arn = aws_iam_role.example.arn
-
-  vpc_config {
-    subnet_ids = [aws_subnet.subnet.id]
-  }
-}
-data "archive_file" "lambda_hello_world" {
-  type        = "zip"
-  source_dir  = "../api/hello-world"
-  output_path = "${path.module}/hello-world.zip"
 }
 resource "aws_iam_role" "lambda_exec" {
   name = "lambda_exec_role"
@@ -175,44 +36,67 @@ resource "aws_iam_role" "lambda_exec" {
     }]
   })
 }
-resource "aws_api_gateway_rest_api" "myapi" {
-  name        = "my_api"
-  description = "API Gateway for my CRUD application"
+data "archive_file" "lambda_hello_world" {
+  type        = "zip"
+  source_dir  = "../api/hello-world"
+  output_path = "${path.module}/hello-world.zip"
 }
-resource "aws_api_gateway_resource" "myresource" {
-  rest_api_id = aws_api_gateway_rest_api.myapi.id
-  parent_id   = aws_api_gateway_rest_api.myapi.root_resource_id
-  path_part   = "myresource"
-}
-
-resource "aws_api_gateway_method" "mymethod" {
-  rest_api_id   = aws_api_gateway_rest_api.myapi.id
-  resource_id   = aws_api_gateway_resource.myresource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-resource "aws_api_gateway_integration" "myintegration" {
-  rest_api_id             = aws_api_gateway_rest_api.myapi.id
-  resource_id             = aws_api_gateway_resource.myresource.id
-  http_method             = aws_api_gateway_method.mymethod.http_method
-  integration_http_method = "POST"
-  type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.hello-word.invoke_arn
-}
-
-resource "aws_lambda_permission" "apigw" {
-  statement_id  = "AllowAPIGatewayInvoke"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.hello-word.function_name
-  principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.myapi.execution_arn}/*/*"
-}
-resource "aws_lambda_function" "hello-word" {
-  function_name    = "HelloWorld"
-  filename         = "hello-world.zip"
+resource "aws_lambda_function" "hello_world" {
+  function_name = "hello-world"
+  role          = aws_iam_role.lambda_exec.arn
   runtime          = "nodejs18.x"
   handler          = "index.handler"
   source_code_hash = data.archive_file.lambda_hello_world.output_base64sha256
-  role             = aws_iam_role.lambda_exec.arn
+  filename = "hello-world.zip"
+}
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name              = "/aws/lambda/hello-world"
+  retention_in_days = 7
+}
+resource "aws_api_gateway_rest_api" "hello_world_api" {
+  name        = "HelloWorldAPI"
+  description = "API to return Hello World"
+}
 
+resource "aws_api_gateway_resource" "hello_world_resource" {
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  parent_id   = aws_api_gateway_rest_api.hello_world_api.root_resource_id
+  path_part   = "hello"
+}
+
+resource "aws_api_gateway_method" "hello_world_method" {
+  rest_api_id   = aws_api_gateway_rest_api.hello_world_api.id
+  resource_id   = aws_api_gateway_resource.hello_world_resource.id
+  http_method   = "GET"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "hello_world_integration" {
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  resource_id = aws_api_gateway_resource.hello_world_resource.id
+  http_method = aws_api_gateway_method.hello_world_method.http_method
+  integration_http_method = "POST"
+  type = "AWS_PROXY"
+  uri = aws_lambda_function.hello_world.invoke_arn
+}
+
+resource "aws_lambda_permission" "api_gateway" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.hello_world.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.hello_world_api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_deployment" "hello_world_deployment" {
+  depends_on = [
+    aws_api_gateway_integration.hello_world_integration,
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.hello_world_api.id
+  stage_name  = "dev"
+}
+
+output "api_url" {
+  value = "http://localhost:4566/restapis/${aws_api_gateway_rest_api.hello_world_api.id}/dev/_user_request_/hello"
 }
